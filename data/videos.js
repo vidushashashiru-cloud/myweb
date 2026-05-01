437dedb5484c4757221f9c849ba56adc44cb87b5
vidushashashiru-cloud <vidushashashiru@gmail.com>
2026-05-01 16:54:31 +0530

    Update videos.js

--- data/videos.js
+++ data/videos.js
@@ -36,7 +36,7 @@ export const videos = [
     recent: true,              // ✅ Recent section හි show කිරීමට true
 
     // ✅ Embed URL - replace VIDEO_ID
-    embed_url: "logo.jpj",
+    embed_url: "logo.jpg",
     embed_allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
 
     // ✅ Thumbnail - YouTube auto thumbnail
